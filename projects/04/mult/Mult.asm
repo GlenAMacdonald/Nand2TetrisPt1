8767ec5bedeb8992c072cca222b38be21a4005a7
Spares <macdonald.glen.a@gmail.com>
2022-12-04 15:13:48 +1100

    Multiply Success

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,53 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+
+@R0 // Read and record the first value
+D=M
+@firstValue
+M=D
+
+@R1 // Read and record the second value
+D=M
+@secondValue
+M=D
+
+@R2 // Set the output to zero
+M=0
+ 
+@loopCount // Initialize the loop counter $Register18
+M=0
+
+@accumulator // Initialize the value accumulator $Register18
+M=0
+
+(LOOP) // to multiply, add a (first number), b (second number) times
+    @secondValue // Retrieve the second number
+    D=M
+    @loopCount // Compare the current loop count to the second number
+    D=D-M
+    @STOP // If they are equal stop accumulating values.
+    D;JEQ 
+
+    @firstValue // else, retrieve the first number
+    D=M
+    @accumulator // add it to the accumulator another time
+    M=M+D;
+        
+    @loopCount // Add 1 to the loop count
+    M=M+1
+    @LOOP // jump to the start of the loop
+    0;JMP
+
+(STOP) // at the end the value needs to be returned
+    @accumulator
+    D=M
+    @R2
+    M=D
+
+(END)
+    @END
+    0; JMP
+    
+
+
